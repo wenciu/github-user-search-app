a650ad440e7c1466b6626b2d8de6437cdc7c0c16
wenciu <72387149+wenciu@users.noreply.github.com>
2023-11-06 20:53:22 +0100

    .

--- src/components/Data_Fetch.jsx
+++ src/components/Data_Fetch.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function Data_Fetch() {
+  const fetchData = async () => {
+    fetch("https://api.github.com/users/wenciu")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return <div>Data_Fetch</div>;
 }
 
--- src/components/User_info.jsx
+++ src/components/User_info.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Data_Fetch from "./Data_Fetch";
 
 function User_info() {
-  return <div>User_info</div>;
+  return (
+    <>
+      <div>User_info</div>
+      <Data_Fetch />
+    </>
+  );
 }
 
 export default User_info;
