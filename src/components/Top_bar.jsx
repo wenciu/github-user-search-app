8a130ca587f3511a876ff8ba16c08db63729bed2
wenciu <72387149+wenciu@users.noreply.github.com>
2023-11-07 08:28:08 +0100

    .

--- src/components/Top_bar.jsx
+++ src/components/Top_bar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Switch_mode from "./Switch_mode";
+import IconSun from "../images/icon-sun.svg";
 
 function Top_bar() {
   return (
     <div className="top-bar">
       <div className="logo">Logo</div>
-      <button className="switch-btn">Light</button>
+      <div className="theme-light">
+        <button className="switch-btn">Light</button>
+        <img src={IconSun} alt="IconSun" />
+      </div>
     </div>
   );
 }
